39e4b450238036c0fbabcfc8aa290b69980fbcce
dullee <91831552+dullee@users.noreply.github.com>
2026-05-04 22:31:43 +0800

    started bitboard

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,73 @@
-let name = "Dulguun";
-let age = "20";
-let adress = "UB";
+// let name = "Dulguun";
+// let age = "20";
+// let adress = "UB";
 
-console.log("Name: " + name + "\nAgeÍÍ:" + age, "\nAdress:", adress);
-const canvas = document.getElementById("canvas");
-canvas.setAttribute("height", "1000px");
-canvas.setAttribute("width", "1000px");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgb(233, 250, 172)";
-ctx.fillRect(0, 0, 800, 800);
-function draw() {
-  let posX = 0;
-  let posY = 0;
-  let skipTile = true;
+// console.log("Name: " + name + "\nAgeÍÍ:" + age, "\nAdress:", adress);
+// const canvas = document.getElementById("canvas");
+// canvas.setAttribute("height", "1000px");
+// canvas.setAttribute("width", "1000px");
+// const ctx = canvas.getContext("2d");
+// ctx.fillStyle = "rgb(233, 250, 172)";
+// ctx.fillRect(0, 0, 800, 800);
+// function draw() {
+//   let posX = 0;
+//   let posY = 0;
+//   let skipTile = true;
 
-  for (let y = 0; y < 8; y++) {
-    for (let x = 0; x < 8; x++) {
-      if (!skipTile) {
-        ctx.fillStyle = "green";
-        ctx.fillRect(posX, posY, 100, 100);
-        posX += 100;
-        skipTile = !skipTile;
-      } else {
-        posX += 100;
-        skipTile = !skipTile;
-      }
+//   for (let y = 0; y < 8; y++) {
+//     for (let x = 0; x < 8; x++) {
+//       if (!skipTile) {
+//         ctx.fillStyle = "green";
+//         ctx.fillRect(posX, posY, 100, 100);
+//         posX += 100;
+//         skipTile = !skipTile;
+//       } else {
+//         posX += 100;
+//         skipTile = !skipTile;
+//       }
+//     }
+//     posY += 100;
+//     posX = 0;
+//     skipTile = !skipTile;
+//   }
+// }
+
+// function placePieces() {
+//   const pawnImg = new Image();
+//   pawnImg.src = "images/pawn.png";
+//   let posX = 0;
+//   let posY = 100;
+//   pawnImg.onload = () => {
+//     for (let y = 0; y < 2; y++) {
+//       for (let x = 0; x < 8; x++) {
+//         ctx.drawImage(pawnImg, posX, posY, 100, 100);
+//         posX += 100;
+//       }
+//       posX = 0;
+//       posY = 600;
+//       ctx.filter = "invert(100%)";
+//     }
+//   };
+// }
+
+function boardTerminal() {
+  var pawnPos =
+    0b0000000011111111000000000000000000000000000000001111111100000000n;
+  let out = "";
+  for (let column = 7; column >= 0; column--) {
+    for (let row = 0; row < 8; row++) {
+      const sq = column * 8 + row;
+      const bit = (pawnPos >> BigInt(sq)) & 1n;
+      out += bit ? "p " : "- ";
     }
-    posY += 100;
-    posX = 0;
-    skipTile = !skipTile;
+    out += `\n`;
   }
+  console.log(out);
 }
+boardTerminal();
 
-function placePieces() {
-  const pawnImg = new Image();
-  pawnImg.src = "images/pawn.png";
-  let posX = 0;
-  let posY = 100;
-  pawnImg.onload = () => {
-    for (let y = 0; y < 2; y++) {
-      for (let x = 0; x < 8; x++) {
-        ctx.drawImage(pawnImg, posX, posY, 100, 100);
-        posX += 100;
-      }
-      posX = 0;
-      posY = 600;
-      ctx.filter = "invert(100%)";
-    }
-  };
-}
-
-draw();
-placePieces();
+// draw();
+// placePieces();
 
 // const header = document.querySelector("h1");
 
